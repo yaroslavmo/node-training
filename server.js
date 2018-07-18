395d69174590b299657efd94e9816ea65f370b5d
Yarik Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2018-07-18 17:34:15 +0300

    response body

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/:name', function (req, res) {
-    let name = req.params.name;
-    res.json({
-        message: `hello ${name}`
-    });
+app.use(express.json());
+
+app.post('/:say', function (req, res) {
+    let say = req.params.say;
+    let {first, last} = req.body;
+    let message = `${say} ${first} ${last}`;
+    res.json({ message });
 });
 
+//
 
 app.listen(3000, function () {
     console.log('Server started port 3000.')
